3b427f98f88cd1bf492bd6ebf30834e76a517838
Mahdi Lamb <mahdilamb@gmail.com>
2020-09-21 13:20:50 +0100

    fix issue when min isn't real min in CollidableBox

---
+++ src/test/java/net/mahdilamb/illuminate/test/ReplicateIssueInRendererTest.java
@@ -0,0 +1,27 @@
+package net.mahdilamb.illuminate.test;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class ReplicateIssueInRendererTest {
+    @Test
+    public void objectShouldBeFoundByCursorTest() {
+        final QuadTree<Integer> test = new QuadTree<>(new float[]{-1, -1}, new float[]{+1, +1}, 4, 50);
+
+        test.insert(
+                new CollidableAABB(new float[]{-0.975f, 0.96875f}, new float[]{-0.85f, 0.8125f}), 2
+        );
+        test.insert(
+                new CollidableAABB(new float[]{-1.0f, 1.0f}, new float[]{0.0f, -0.25f}), 3
+        );
+        test.insert(
+                new CollidableAABB(new float[]{0.0f, 1.0f}, new float[]{0.0f, -0.25f}), 5
+        );
+        assertEquals(test.getObjectsContaining(-0.9207161f, 0.8735441f).size(), 2);
+    }
+
+}
